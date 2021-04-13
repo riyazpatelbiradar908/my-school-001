4d96d2e9b730de0a6d47af3712fbdead1b93d12c
riyazpatelbiradar908 <48643243+riyazpatelbiradar908@users.noreply.github.com>
2021-04-13 17:01:50 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,29 +2,13 @@
 session_start();
 require_once('apiHeader.php');
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if($username === 'admin' && $password === 'admin') {
-        $res = [
-            'status' => 200,
-            'token' => session_id(),
-            'message' => 'successfully logged in'
-        ];
-    } else {
-        $res = [
-            'status' => 404,
-            'message' => 'Invalid credentials'
-        ];
-    }
-    
-} else {
-    $res = [
-        'status' => 404,
-        'message' => 'Invalid credentials'
-    ];
-}
+$res = [
+    'status' => 200,
+    'token' => session_id(),
+    'message' => 'successfully logged in',
+    'server' => $_SERVER,
+    'post' => $_POST
+];
 
 header('Content-Type: application/json');
 http_response_code($res['status']);
